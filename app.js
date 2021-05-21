d041e001965c99d215730b3dd691d295a9babd7a
changjandam <changjandamtw@gmail.com>
2021-05-21 17:22:15 +0800

    project init

---
+++ app.js
@@ -0,0 +1,15 @@
+const express = require('express')
+const exphbs = require('express-handlebars')
+const bodyParser = require('body-parser')
+const methodOverrride = require('method-override')
+
+const app = express()
+const PORT = 3000
+
+app.use(bodyParser.urlencoded({ extended: true }))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
+
+app.set('view engine', 'hbs')
+app.listen(PORT, () => {
+  console.log(`The web is running on http://localhost:${PORT}`)
+})
